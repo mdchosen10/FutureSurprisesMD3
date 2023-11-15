fddea79906d089d700aef4d143323e1ce0b83c01
mdchosen10 <143424070+mdchosen10@users.noreply.github.com>
2023-11-15 01:11:43 -0600

    type error fix

--- src/types/react-stepper-horizontal.d.ts
+++ src/types/react-stepper-horizontal.d.ts
@@ -2,3 +2,5 @@ declare module "react-stepper-horizontal";
 declare module "react-datepicker";
 declare module "qs";
 declare module "js-cookie";
+declare module "redux";
+declare module "react-hot-toast";
